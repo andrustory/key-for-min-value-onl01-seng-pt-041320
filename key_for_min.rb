9572757af3c41607008cc63f9e917500a407fd2c
Andres Carvajal <jc1993box@hotmail.com>
2020-04-29 01:54:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,41 +7,9 @@ def key_for_min_value(name_hash)
   lowest_value={}
   nil
   if value < smallest_value
-      lowest_value = value
-      lowest_key = key
+      smallest_value = value
+      smallest_key = key
     end
   end
-  lowest_key
-  if name_hash = {}
-        return nil
-    name_hash.sort{|key, value|key<=>value}
-        return key.first[0]
-end
-end
-
-
-# lowest_key = nil
-# end 		   lowest_value = nil
-#   name_hash.collect do |key, value|
-#     if lowest_value == nil || value < lowest_value
-#       lowest_value = value
-#       lowest_key = key
-#     end
-#   end
-#   lowest_key
-
-
-# lowest_value = 0 
-#   lowest_key = nil 
-
-#     name_hash.collect do |key, value|
-#     if lowest_value == 0 || value < lowest_value
-#       lowest_value = value
-#       lowest_key = key
-#     end
-#   end
-# if name_hash = {}
-#     nil
-#     name_hash.sort do |key, value|
-#     key <=> value
-#   end.first[0]
+  smallest_key
+ 
