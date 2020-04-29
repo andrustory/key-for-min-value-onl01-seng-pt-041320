3b213493cae80354fc541b1cc347e17ac7cf24f1
Andres Carvajal <jc1993box@hotmail.com>
2020-04-29 01:49:20 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,14 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-# def key_for_min_value(name_hash)
-#   smallest_value = {}
-#   if name_hash = {}
-#         return nil
-#     name_hash.sort{|key, value|key<=>value}
-#         return key.first[0]
-# end
-# end
+def key_for_min_value(name_hash)
+  smallest_value = {}
+  if name_hash = {}
+        return nil
+    name_hash.sort{|key, value|key<=>value}
+        return key.first[0]
+end
+end
 
 
 # lowest_key = nil
